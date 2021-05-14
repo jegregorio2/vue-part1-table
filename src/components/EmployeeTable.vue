881c223813696a7729a5db380b1e755d80c923b7
Juan <juan.e.gregorio@gmail.com>
2021-05-13 20:50:41 -0700

    Initial commit

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- add table -->
+  <div id="employee-table">
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Richard Hendricks</td>
+          <td>richard@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Bertram Gilfoyle</td>
+          <td>gilfoyle@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Dinesh Chugtai</td>
+          <td>dinesh@piedpiper.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'EmployeeTable',
+  }
+</script>
+
+<style scoped></style>
